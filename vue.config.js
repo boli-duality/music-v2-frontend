6f394a9d8416038e3a266596540d884355fdcfe7
boliyou <2454462834@qq.com>
2022-11-21 04:31:16 +0800

    chore: 修改全局注入构建

--- vue.config.js
+++ vue.config.js
@@ -7,11 +7,15 @@ module.exports = defineConfig({
       args[0].title = '音乐的力量'
       return args
     })
-  },
-  pluginOptions: {
-    'style-resources-loader': {
-      preProcessor: 'scss',
-      patterns: [path.resolve(__dirname, './src/common/styles/mixins.scss')],
-    },
+    // explain 样式全局注入 https://blog.csdn.net/qq_37831545/article/details/126315428
+    config.module.rule('scss').oneOfs.store.forEach(item => {
+      item
+        .use('style-resources-loader')
+        .loader('style-resources-loader')
+        .options({
+          patterns: [path.resolve(__dirname, './src/common/styles/mixins.scss')],
+        })
+        .end()
+    })
   },
 })
