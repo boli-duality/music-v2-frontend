f8c9bf272f93474df6ca3799cd71925803f4151e
pengpeng <1907735524@qq.com>
2022-11-20 21:26:52 +0800

    feat: 新增侧边栏

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="main">shouye</div>
+  <div class="w">
+    <div class="nav">
+      <div class="navlist">
+        <div>发现音乐</div>
+        <div>播客</div>
+        <div>视频</div>
+        <div>关注</div>
+        <div>直播</div>
+        <div>私人FM</div>
+      </div>
+      <div class="mymusic">
+        <div class="head">我的音乐</div>
+        <div class="fav">我喜欢的音乐</div>
+        <div class="loc">本地与下载</div>
+        <div class="new">最近播放</div>
+      </div>
+      <div class="musiclist">创建的歌单</div>
+    </div>
+    <div class="main">首页</div>
+  </div>
 </template>
 
 <script>
@@ -9,7 +28,57 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.main {
+.w {
+  display: flex;
   height: 537px;
+}
+.nav {
+  width: 200px;
+  border-right: 1px solid #e0e0e0;
+}
+.navlist {
+  height: 256px;
+  padding: 12px 0 0 12px;
+}
+.navlist div {
+  height: 36px;
+  margin-bottom: 5px;
+  padding-left: 7px;
+  font-size: 15px;
+  line-height: 36px;
+  background-color: #f6f6f7;
+}
+.mymusic {
+  padding-left: 12px;
+}
+.head {
+  height: 21px;
+  font-size: 14px;
+  color: #9f9f9f;
+}
+.fav {
+  height: 38px;
+  padding-left: 8px;
+  line-height: 38px;
+  background-color: #f6f6f7;
+}
+.loc {
+  height: 38px;
+  padding-left: 8px;
+  line-height: 38px;
+  background-color: #f6f6f7;
+}
+.new {
+  height: 38px;
+  padding-left: 8px;
+  line-height: 38px;
+  background-color: #f6f6f7;
+}
+.musiclist {
+  height: 148px;
+  padding-left: 25px;
+  padding-top: 16px;
+  font-size: 14px;
+  color: #9f9f9f;
 }
 </style>
