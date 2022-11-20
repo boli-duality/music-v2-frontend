4ca63d7d625f8f7a860806a3fd7fff98ffb63f89
boliyou <2454462834@qq.com>
2022-11-21 04:31:38 +0800

    feat: 增加响应拦截

--- src/common/request/index.js
+++ src/common/request/index.js
@@ -3,6 +3,23 @@ import axios from 'axios'
 // import store from '@/store'
 // import router from '@/router'
 
+// 服务器错误处理
+const errMsg = {
+  [400]: '请求参数错误 400',
+  [401]: '未授权，请登录 401',
+  [403]: '拒绝访问 403',
+  [404]: '请求地址不存在 404',
+  [405]: '请求方法不允许 405',
+  [408]: '请求超时 408',
+  [415]: '不支持的媒体类型(一般来说是content-type错了) 415',
+  [500]: '服务器内部错误 500',
+  [501]: '服务未实现 501',
+  [502]: '网关错误 501',
+  [503]: '服务不可用 503',
+  [504]: '网关超时 504',
+  [505]: 'HTTP版本不受支持 505',
+}
+
 // create an axios instance
 const request = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
@@ -31,36 +48,42 @@ const request = axios.create({
 // )
 
 // response interceptor
-// request.interceptors.response.use(
-//   /**
-//    * If you want to get http information such as headers or status
-//    * Please return  response => response
-//    */
+request.interceptors.response.use(
+  /**
+   * If you want to get http information such as headers or status
+   * Please return  response => response
+   */
 
-//   /**
-//    * Determine the request status by custom code
-//    * Here is just an example
-//    * You can also judge the status by HTTP Status Code
-//    */
-//   response => {
-//     const { success, data, message } = response.data
+  /**
+   * Determine the request status by custom code
+   * Here is just an example
+   * You can also judge the status by HTTP Status Code
+   */
+  response => {
+    const {
+      status,
+      data: { code, ...data },
+    } = response
 
-//     if (success) {
-//       return data
-//     } else {
-//       Message.error(message)
-//       return Promise.reject(new Error('请求数据失败!'))
-//     }
-//   },
-//   error => {
-//     console.log('err' + error) // for debug
-//     Message({
-//       message: error.message,
-//       type: 'error',
-//       duration: 5 * 1000
-//     })
-//     return Promise.reject(error)
-//   }
-// )
+    if (status == 200) {
+      switch (code) {
+        case 200:
+          return Object.values(data)[0]
+        case 2:
+          // 执行登录操作
+          break
+      }
+    } else throw new Error(errMsg[status])
+  }
+  // ,error => {
+  //   console.log('err' + error) // for debug
+  //   Message({
+  //     message: error.message,
+  //     type: 'error',
+  //     duration: 5 * 1000,
+  //   })
+  //   return Promise.reject(error)
+  // }
+)
 
 export default request
