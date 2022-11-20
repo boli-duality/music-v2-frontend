01995f35094b1b13b6841f2d39cad8fafda5007d
boliyou <2454462834@qq.com>
2022-11-20 19:11:34 +0800

    feat: 头部和底部组件化

---
+++ src/views/Layout/components/AppHeader/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <header class="app-header">
+    <div class="logo">
+      <div class="logo-icon"><i class="iconfont icon-ic_music"></i></div>
+      <span class="logo-title">音乐的力量</span>
+    </div>
+    <div class="search">搜索框</div>
+    <div class="setting">用户登入</div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+}
+</script>
+
+<style lang="scss" scoped>
+.app-header {
+  display: flex;
+  height: 60px;
+  background: var(--theme-red);
+  color: #fff;
+  .logo {
+    display: flex;
+    padding-left: 19px;
+    width: 229px;
+    align-items: center;
+    &-icon {
+      @include flex-center;
+      margin-right: 6px;
+      width: 24px;
+      height: 24px;
+      border-radius: 50%;
+      background: #fff;
+      color: var(--theme-red);
+      .icon-ic_music {
+        font-size: 14px;
+      }
+    }
+    &-title {
+      font-size: 16px;
+    }
+  }
+  .search {
+    height: 60px;
+    width: 420px;
+    background: pink;
+  }
+}
+</style>
