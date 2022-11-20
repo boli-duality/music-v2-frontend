70da192ca458ed4d6f4436db75fbddb361633482
boliyou <2454462834@qq.com>
2022-11-20 19:09:57 +0800

    chore: 样式文件全局注入

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
+const path = require('path')
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: [path.resolve(__dirname, './src/common/styles/mixins.scss')],
+    },
+  },
 })
