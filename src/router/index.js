3fe66115936619ba814e5866673728c9f4b6a393
boliyou <2454462834@qq.com>
2022-11-21 22:02:33 +0800

    feat: todo

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,10 @@ const routes = [
         alias: '/',
         path: 'home',
         name: 'home',
+        // TODO 使用插件动态更换网页标题
+        meta: {
+          title: '首页',
+        },
         component: () => import(/* webpackChunkName: "home" */ '@/views/home/index.vue'),
       },
     ],
