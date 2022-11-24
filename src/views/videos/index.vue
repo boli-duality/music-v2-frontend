40be954989b60b5af63caa0f85fecb0f0cb403fc
boliyou <2454462834@qq.com>
2022-11-24 08:36:57 +0800

    feat: 新增视频页

---
+++ src/views/videos/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="videos">视频页</div>
+</template>
+
+<script>
+export default {
+  name: 'videos',
+  created() {},
+}
+</script>
+
+<style lang="scss" scoped>
+.videos {
+}
+</style>
