879ef0c7b2576d1bb20a50ba512d4a2ccbb802b2
youbolin <youbolin001@163.com>
2022-11-29 10:53:19 +0800

    feat: 新增本地与下载、最近播放页

---
+++ src/views/Layout/recentlyPlayed/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="recently-played">最近播放</div>
+</template>
+
+<script>
+export default {
+  name: 'RecentlyPlayed',
+}
+</script>
+
+<style lang="scss" scoped>
+.recently-played {
+}
+</style>
