7627ad2a0691f196682f44ac8a610d7caec6f166
boliyou <youbolin001@163.com>
2022-12-02 03:28:38 +0800

    feat: 全局注册拖拽组件

--- src/common/config/index.js
+++ src/common/config/index.js
@@ -5,6 +5,14 @@ import './element-ui'
 import request from '@/common/request'
 // 动态设置网页标题 https://github.com/deboyblog/vue-wechat-title
 import vueTitle from 'vue-wechat-title'
+// 拖拽改变大小组件
+import VueDragResize from 'vue-draggable-resizable'
+// optionally import default styles
+import 'vue-draggable-resizable/dist/VueDraggableResizable.css'
 
+// 使用插件
 Vue.use(vueTitle) // 动态设置网页标题
+// 注册全局组件
+Vue.component('VueDragResize', VueDragResize)
+// 原型链
 Vue.prototype.$_http = request // 请求
