536e1a02f5ec571a6563349f4db7a13e1012fd37
youbolin <youbolin001@163.com>
2022-12-02 10:07:45 +0800

    chore: 修改变量名

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -3,12 +3,12 @@
     class-name="music-layout-drap"
     drag-handle=".app-header"
     drag-cancel=".music-layout-drag-cancel"
-    :w="reSize.w"
-    :h="reSize.h"
-    :min-width="reSize.w"
-    :min-height="reSize.h"
-    :x="dragPosition.x"
-    :y="dragPosition.y"
+    :w="size.w"
+    :h="size.h"
+    :min-width="size.w"
+    :min-height="size.h"
+    :x="position.x"
+    :y="position.y"
     :handles="['mr', 'tl', 'bl', 'tr', 'br']"
     :onDrag="onDrag"
   >
@@ -37,13 +37,10 @@ export default {
   data() {
     return {
       draggable: true,
-      dragPosition: { x: 0, y: 0 },
-      reSize: {
-        w: 1022,
-        h: 670,
-      },
-      // dragRange:{
-      //   top:this.dragPosition.
+      position: { x: 0, y: 0 },
+      size: { w: 1022, h: 670 },
+      // range:{
+      //   top:this.position.
       // }
     }
   },
@@ -56,10 +53,10 @@ export default {
   methods: {
     setInitPositon() {
       const { clientWidth, clientHeight } = document.documentElement
-      const centerX = clientWidth / 2 - this.reSize.w / 2
-      const centerY = clientHeight / 2 - this.reSize.h / 2 - 52
-      this.dragPosition.x = centerX > 0 ? centerX : 0
-      this.dragPosition.y = centerY > 0 ? centerY : 0
+      const centerX = clientWidth / 2 - this.size.w / 2
+      const centerY = clientHeight / 2 - this.size.h / 2 - 52
+      this.position.x = centerX > 0 ? centerX : 0
+      this.position.y = centerY > 0 ? centerY : 0
     },
     onDrag(x, y) {
       console.log('x: ', x)
@@ -67,7 +64,7 @@ export default {
       console.log(x <= -100)
       // -1018
       if (x <= -1018 || y <= -59) return false
-      // if (x<=-this.reSize.w)
+      // if (x<=-this.size.w)
       // this.x = x
       // this.y = y
     },
