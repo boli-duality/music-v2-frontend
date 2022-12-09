b5d9efc7bb8493c709879bf40fc1e498a76e8566
youbolin <youbolin001@163.com>
2022-12-09 18:05:09 +0800

    chore: 更新工具函数扩展引用

--- src/common/index.js
+++ src/common/index.js
@@ -1,7 +1,7 @@
 // 通用样式
 import './styles/index.scss'
 // 按需引入element-ui
-import elementUi from './third-components/element-ui'
+import elementUI from './third-components/element-ui'
 // 拖拽改变大小组件
 // import VueDragResize from 'vue-draggable-resizable'
 // import 'vue-draggable-resizable/dist/VueDraggableResizable.css' // optionally import default styles
@@ -11,16 +11,13 @@ import request from './request'
 // 动态设置网页标题 https://github.com/deboyblog/vue-wechat-title
 import vueTitle from 'vue-wechat-title'
 // 扩展原生js
-import { copy, logs } from './utils/functions'
-
-// 扩展原生js
-JSON.copy = copy // JSON深拷贝
-console.logs = logs // 带颜色标签的打印信息
+import { vuePlugin as functions } from './utils/functions'
 
 const install = Vue => {
   // 使用插件
   Vue.use(vueTitle) // 动态设置网页标题
-  Vue.use(elementUi) // 按需引入element-ui
+  Vue.use(elementUI) // 按需引入element-ui
+  Vue.use(functions) // 工具函数扩展
   // 注册全局组件
   Vue.component('VueDragResize', VueDragResize) // 拖拽改变大小组件
   // 原型链
